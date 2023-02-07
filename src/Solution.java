7b454439a01fc9f2f6fd69ddfd0648af9f562d62
AishwaryaT19 <aishwarya.tewari19@gmail.com>
2023-02-07 23:57:40 +0530

    first program

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Solution {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int a = maxProfit(arr);
+        System.out.println(a);
+    }
+
+    public static int maxProfit(int[] prices) {
+        int max = 0;
+
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                if (max < (prices[j] - prices[i])) {
+                    max = prices[j] - prices[i];
+                }
+            }
+        }
+        return max;
     }
 }
